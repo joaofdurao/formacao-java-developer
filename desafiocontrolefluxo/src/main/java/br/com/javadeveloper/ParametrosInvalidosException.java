34c678e4e5ec0a80e546f51fa914c5ac19f69c02
joaofdurao <devjoaofelipe.dlc@gmail.com>
2025-07-02 20:49:28 -0300

    Desafio do contador

---
+++ desafiocontrolefluxo/src/main/java/br/com/javadeveloper/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package br.com.javadeveloper;
+
+public class ParametrosInvalidosException extends Exception {
+
+    public ParametrosInvalidosException() {   }
+    
+    public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+
+
+}
