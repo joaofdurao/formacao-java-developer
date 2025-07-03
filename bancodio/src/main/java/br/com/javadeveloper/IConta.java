ff4510f764b569fcc02e642fb55425188fe9d4c0
joaofdurao <devjoaofelipe.dlc@gmail.com>
2025-07-02 21:09:01 -0300

    Subindo desafio banco

---
+++ bancodio/src/main/java/br/com/javadeveloper/IConta.java
@@ -0,0 +1,11 @@
+package br.com.javadeveloper;
+
+public interface IConta {
+	void sacar(double valor);
+	
+	void depositar(double valor);
+	
+	void transferir(double valor, IConta contaDestino);
+	
+	void imprimirExtrato();
+}
